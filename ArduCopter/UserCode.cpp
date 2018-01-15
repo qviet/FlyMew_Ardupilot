dd56bb84e3b8af348394a4f88ba1ae4af07a13b0
nguyenquocvietbmt1996@gmail.com <nguyenquocvietbmt1996@gmail.com>
2018-01-16 03:53:44 +0700

    gcs missionplaner

--- ArduCopter/UserCode.cpp
+++ ArduCopter/UserCode.cpp
@@ -33,12 +33,13 @@ void Copter::userhook_FastLoop()
         }
         else if(ips_char[0] == 'e'){
             // end-of-frame: get ips_pos & time_stamp
-            if((ips_char[4] ==',') && (ips_char[8] ==',') && (c_buff == 12)){
+            if((ips_char[4] ==',')&& (c_buff == 8)){
                 // valid frame
                 ips_data[0] = (ips_char[1]-0x30)*100 + (ips_char[2]-0x30)*10 + (ips_char[3]-0x30); //pos_x
                 ips_data[1] = (ips_char[5]-0x30)*100 + (ips_char[6]-0x30)*10 + (ips_char[7]-0x30); //pos_y
                 th_roll = ips_data[0];
                 th_pitch = ips_data[1];
+                copter.gcs_send_text_fmt(MAV_SEVERITY_INFO,"%d,%d",ips_data[0], ips_data[1]);
                 //hal.uartF->printf("%d,%d,%d,%d",ips_data[0],ips_data[1],ips_data[2],ips_data[3]);
                 
             }
